d920471346f5561884424a2dc4d46d4633323094
Acer <magasov94@list.ru>
2022-11-30 16:19:08 +0400

    update constant. update read_file()

--- csv_handler.py
+++ csv_handler.py
@@ -1,21 +1,25 @@
 import csv
 from pathlib import Path
 
-from config import CATEGORY_FILE_NAME, OUTPUT_DIRECTORY
+from config import CATEGORY_FILE_PATH
 
 
-class Category:
-    FILEDIR = Path(__file__).parent / OUTPUT_DIRECTORY / CATEGORY_FILE_NAME
-    Path.mkdir(FILEDIR.parent, exist_ok=True)
-
-    def create_file(self, category):
-        with open(self.FILEDIR,  "w", newline="", encoding='utf-8') as file:
+class CategoryHandler:
+    @staticmethod
+    def create_file(category):
+        with open(CATEGORY_FILE_PATH,  "w", newline="", encoding='utf-8') as file:
             columns = list(category[0].keys())
             writer = csv.DictWriter(file, fieldnames=columns, delimiter=";")
             writer.writeheader()
 
             writer.writerows(category)
 
-    def read_file(self):
-        with open(self.FILEDIR, "r", newline="", encoding='utf-8') as file:
-            return csv.DictReader(file, delimiter=";")
+    @staticmethod
+    def read_file():
+        data_file = []
+        with open(CATEGORY_FILE_PATH, "r", newline="") as file:
+            reader = csv.DictReader(file, delimiter=";")
+            for category in reader:
+                data_file.append(dict(category))
+
+        return data_file
