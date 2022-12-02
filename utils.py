0638d3545e3409a2b6cca35c4a14915867005ea7
Acer <magasov94@list.ru>
2022-12-02 17:05:49 +0400

    add get_delay_value func.

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 
 
 def delete_duplicate_product(products: List[dict]) -> List[dict]:
@@ -12,3 +12,16 @@ def delete_duplicate_product(products: List[dict]) -> List[dict]:
     products_dict = {i["sku_article"] + i["sku_barcode"]: i for i in products}
 
     return list(products_dict.values())
+
+
+def get_delay_value(delay_range: str) -> Union[list, None]:
+    """Формирует данные задержки из переданного параметра delay_range_s в файле конфигурации.
+
+    Args:
+        delay_range: строка с данными из файла конфигурации
+    Return:
+        Union[list, None]: список с min/max значением задержки, либо None, если значение не передано или равно нулю
+    """
+    if delay_range and delay_range not in [0, "0"]:
+        delay_range = delay_range.split("-")
+        return [int(delay_range[0]), int(delay_range[-1])]
